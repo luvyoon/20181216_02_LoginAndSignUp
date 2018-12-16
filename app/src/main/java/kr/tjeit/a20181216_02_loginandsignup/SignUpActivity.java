8b6f5e826f8349084c40399d9f9a25f78cae1de5
Hottory <gkffndnl123@naver.com>
2018-12-16 14:47:13 +0900

    약관동의 화면 그리기

--- app/src/main/java/kr/tjeit/a20181216_02_loginandsignup/SignUpActivity.java
+++ app/src/main/java/kr/tjeit/a20181216_02_loginandsignup/SignUpActivity.java
@@ -1,6 +1,5 @@
 package kr.tjeit.a20181216_02_loginandsignup;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SignUpActivity extends BaseActivity {
@@ -10,5 +9,20 @@ public class SignUpActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
     }
 }
