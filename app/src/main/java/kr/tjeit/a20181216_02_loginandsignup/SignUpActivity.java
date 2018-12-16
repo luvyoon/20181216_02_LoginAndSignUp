c5eb725f5c9d371fd3ad38caee3079a258ff9b5a
Hottory <gkffndnl123@naver.com>
2018-12-16 14:18:30 +0900

    로그인화면 그리기 완료

---
+++ app/src/main/java/kr/tjeit/a20181216_02_loginandsignup/SignUpActivity.java
@@ -0,0 +1,14 @@
+package kr.tjeit.a20181216_02_loginandsignup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpActivity extends BaseActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
